3caf01dcac66fc79e4c09ba490e2e401bf843dd7
Andrew Kim <taeyeonkim90@gmail.com>
2017-02-20 12:57:22 -0800

    put stubs for nussinov's algorithm

--- code/fold.cpp
+++ code/fold.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 extern "C" {
 	#include "foldingc.h"	//This library will contain all C functions
@@ -8,7 +11,20 @@ extern "C" {
 
 int main() //Let the program be run with input arguments. 1=Nussinov 2=Two Vector no arguments=ask the user
 {
-  std::cout << "Hello World!\n";
+	std::cout << "Hello World!\n";
+
+	string sequence = "UACGCGUCAAUAACGCUA";
+	int seq_size = sequence.size();
+
+	int **score_table = new int*[seq_size];
+	for (int i=0; i<seq_size; i++){
+		score_table[i] = new int[seq_size];
+	}
+
+	int **traceback_table = new int*[seq_size];
+	for (int i=0; i<seq_size; i++){
+		traceback_table[i] = new int[seq_size];
+	}
   
   //Read input sequence
   //if input==1, call Nussinov functions using input sequence.
--- code/foldingcpp.h
+++ code/foldingcpp.h
@@ -3,4 +3,11 @@
 
 //various c++ function declarations
 
+// nussinov
+void init_score_table(int** score_table); // 4 diagonal rows initialized as 0
+void init_traceback_table(int** traceback_table);	//-2 for traceback end point
+void nussinov(string sequence, int** score_table, int** traceback_table); // -1 for diagonal, store k for traceback
+void traceback(string sequence, int** score_table, int** traceback_table, int* match_table);
+
+
 #endif
